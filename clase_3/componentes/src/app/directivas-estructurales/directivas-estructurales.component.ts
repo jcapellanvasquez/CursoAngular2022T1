71a28316031816e29cb6ae612bf201c17aa9125e
jcapellanvasquez <jv.capellan06@gmail.com>
2022-02-07 21:02:56 -0400

    ejemplos de property binding y uso del ngIf

---
+++ clase_3/componentes/src/app/directivas-estructurales/directivas-estructurales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directivas-estructurales',
+  templateUrl: './directivas-estructurales.component.html',
+  styleUrls: ['./directivas-estructurales.component.css']
+})
+export class DirectivasEstructuralesComponent implements OnInit {
+  mostrar = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
